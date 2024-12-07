290051c28b45820a0de4f549078eb1dc447dcc5e
Ryan Scott <rscott5@gmail.com>
2024-12-07 07:04:15 -0500

    2024 day 7

---
+++ 2024/7/day7.rb
@@ -0,0 +1,44 @@
+input = File.open("input.txt").readlines.map(&:chomp)
+
+def evaluate_possibilities(nums, allow_concat: false)
+  num1, num2, *remaining_nums = nums
+
+  combinations = [num1 + num2, num1 * num2]
+  if allow_concat
+    combinations << (num1.to_s + num2.to_s).to_i
+  end
+
+  return combinations if remaining_nums.empty?
+
+  combinations.map do |new_num|
+    evaluate_possibilities([new_num] + remaining_nums, allow_concat:)
+  end.flatten
+end
+
+def is_valid(test_value, nums, allow_concat: false)
+  evaluate_possibilities(nums, allow_concat:).any? { |result| result == test_value }
+end
+
+puts "Part 1:"
+
+tally = input.sum do |row|
+  test_value, nums = row.split(": ")
+  test_value = test_value.to_i
+  nums = nums.split(" ").map(&:to_i)
+
+  is_valid(test_value, nums) ? test_value : 0
+end
+
+puts tally
+
+puts "Part 2:"
+
+tally = input.sum do |row|
+  test_value, nums = row.split(": ")
+  test_value = test_value.to_i
+  nums = nums.split(" ").map(&:to_i)
+
+  is_valid(test_value, nums, allow_concat: true) ? test_value : 0
+end
+
+puts tally
